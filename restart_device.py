e527cc6a87bd425dd28eaaed7c75dd5887058c51
anditsung <anditsung@gmail.com>
2021-06-09 11:26:55 +0700

    update

--- restart_device.py
+++ restart_device.py
@@ -3,9 +3,9 @@
 from zk import ZK, const
 
 devices = {
-    # "Parkiran Pabrik C" : "172.16.56.7",        # CANT REACH
-    # "H Proyek" : "172.16.56.8",                 # CANT REACH
-    # "Pabrik A Sortasi" : "172.16.56.10",        # CANT REACH
+    "Parkiran Pabrik C" : "172.16.56.7",        # CANT REACH
+    "H Proyek" : "172.16.56.8",                 # CANT REACH
+    "Pabrik A Sortasi" : "172.16.56.10",        # CANT REACH
     "Pabrik E 1" : "192.168.15.20",             #   OK
     "Pabrik E 2" : "192.168.15.21",             #   OK
     "Pabrik E Sortasi" : "192.168.15.22",       #   OK
@@ -20,6 +20,8 @@ devices = {
     "Pabrik D Sortasi" : "192.168.2.22",        #   OK
 }
 
+test = True
+
 for device in devices:
     conn = None
     print('Device ' + device + ' on IP ' + devices[device])
@@ -27,13 +29,15 @@ for device in devices:
     try:
         zk = ZK(devices[device])
         conn = zk.connect()
-        print("Device Name: " + conn.get_device_name())
-        print("Platform: " + conn.get_platform())
-        print("Firmware: " + conn.get_firmware_version())
-        print("Serial Number: " + conn.get_serialnumber())
-        # print('Restarting device...')
-        # conn.restart()
-        print('Done restart')
+        if test:
+            print("Device Name: " + conn.get_device_name())
+            print("Platform: " + conn.get_platform())
+            print("Firmware: " + conn.get_firmware_version())
+            print("Serial Number: " + conn.get_serialnumber())
+        else:
+            print('Restarting device...')
+            conn.restart()
+            print('Done restart')
     except Exception as e:
         print('Restart status: ' + str(e))
     
